00158737406ab519704edcd7ab02cbe8ff874137
im94 <ivan@vvm.agency>
2025-06-04 11:48:30 +0200

    cors update

--- multi-step-form-backend-api/router.js
+++ multi-step-form-backend-api/router.js
@@ -2,7 +2,18 @@ const apiRouter = require('express').Router();
 const userController = require('./controllers/userController');
 const cors = require('cors');
 
-apiRouter.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'http://localhost:4000', 'https://imd94.github.io'];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+apiRouter.use(cors(corsOptions));
 
 apiRouter.get('/', (req, res) => res.json('Hello, from backend!'));
 apiRouter.post('/register', userController.apiRegister);
